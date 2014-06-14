a0c7f66049beb556b2d3d60e802bac2837dc9752
Gabe Berke-Williams <gabebw@gabebw.com>
2014-06-14 10:59:50 -0400

    We can respond to GET requests

--- my-sinatra.rb
+++ my-sinatra.rb
@@ -1,8 +1,38 @@
-require 'rack'
+require 'bundler/setup'
+Bundler.require
 
-class HelloWorld
-  def call(env)
-    [200, {"Content-Type" => "text/html"}, "Hello Rack!"]
+class MySinatra
+  class Base
+    def self.call(env)
+      verb = env["REQUEST_METHOD"]
+      path = env["REQUEST_PATH"]
+      get_handler("#{verb} #{path}")
+    end
+
+    def self.get(path, &block)
+      add_handler("GET #{path}", block)
+    end
+
+    def self.add_handler(verb_and_path, block)
+      @handlers ||= {}
+      @handlers[verb_and_path] = block
+    end
+
+    def self.get_handler(verb_and_path)
+      if @handlers.key?(verb_and_path)
+        @handlers[verb_and_path].call
+      else
+        unless path = '/favicon.ico'
+          raise "No handler for #{verb_and_path}"
+        end
+      end
+    end
+  end
+end
+
+class App < MySinatra::Base
+  get "/hello" do
+    [200, {"Content-Type" => "text/html"}, "Hi there!"]
   end
 end
 
